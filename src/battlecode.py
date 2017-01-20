379838b8c071d71c121b74f9588c8e7ba569c71a
David Gayerie <dagaydevel@free.fr>
2017-01-20 18:54:01 +0100

    fix conformité pep-8

--- src/battlecode.py
+++ src/battlecode.py
@@ -1,15 +1,17 @@
 from enum import Enum
 
+
 class TypePieceDesc(object):
     """Provides the description of any kind of piece
 
-    Attibutes:
+    Attributes:
         name The name of the Piece
         attack_points The amount of attack points
     """
     def __init__(self, name, attack_points):
         self.name = name
         self.attack_points = attack_points
+
 
 class TypePiece(Enum):
     """A complete enumeration of all kind of pieces"""
@@ -24,36 +26,39 @@ class TypePiece(Enum):
     TRAP = TypePieceDesc("trap", 0)
     TREASURE = TypePieceDesc("treasure", 0)
 
+
 class Piece(object):
     """A playable piece"""
 
-    def __init__(self, typePiece):
-        self.type = typePiece
+    def __init__(self, type_piece):
+        self.type = type_piece
+
     @property
     def attack_points(self):
         """The amount of attack points"""
         return self.type.value.attack_points
 
+
 def fight(attacker, defender):
     """Determines who wins a fight between two pieces"""
 
-    if defender.type == TypePiece.DRAGON and attacker.type == TypePiece.SLAYER: # if DRAGON vs SLAYER
+    if defender.type == TypePiece.DRAGON and attacker.type == TypePiece.SLAYER:  # if DRAGON vs SLAYER
         looser = defender
     elif defender.type != TypePiece.TRAP and defender.type != TypePiece.TREASURE:
-        if attacker.attack_points > defender.attack_points: # If attacker wins
+        if attacker.attack_points > defender.attack_points:  # If attacker wins
             looser = defender
-        elif attacker.attack_points < defender.attack_points: # If defender looses
+        elif attacker.attack_points < defender.attack_points:  # If defender looses
             looser = attacker
-        else: # If equality
+        else:  # If equality
             looser = None
-    elif defender.type == TypePiece.TRAP: # If defender is a TRAP
-        if attacker.type == TypePiece.DWARF: # If attacker is a DWARF then he wins
+    elif defender.type == TypePiece.TRAP:  # If defender is a TRAP
+        if attacker.type == TypePiece.DWARF:  # If attacker is a DWARF then he wins
             looser = defender
         else:
 
             # If attacker.type is not a DWARF then defender wins
             looser = attacker
     else:
-        looser = defender # TREASURE
+        looser = defender  # TREASURE
 
     return looser
--- src/test/test_battlecode.py
+++ src/test/test_battlecode.py
@@ -1,21 +1,22 @@
 from battlecode import Piece, TypePiece, fight
 
+
 def test_fight():
     p1 = Piece(TypePiece.SORCERESS)
     p2 = Piece(TypePiece.KNIGHT)
-    assert fight(p1,p2) == p1
-    assert fight(p2,p1) == p1
+    assert fight(p1, p2) == p1
+    assert fight(p2, p1) == p1
     p1 = Piece(TypePiece.DRAGON)
     p2 = Piece(TypePiece.TRAP)
-    assert fight(p1,p2) == p1
+    assert fight(p1, p2) == p1
     p1 = Piece(TypePiece.DWARF)
-    assert fight(p1,p2) == p2
+    assert fight(p1, p2) == p2
     p2 = Piece(TypePiece.DWARF)
-    assert fight(p1,p2) == None
-    assert fight(p2,p1) == None
+    assert fight(p1, p2) is None
+    assert fight(p2, p1) is None
     p2 = Piece(TypePiece.TREASURE)
-    assert fight(p1,p2) == p2
+    assert fight(p1, p2) == p2
 
     p23 = Piece(TypePiece.DRAGON)
     p24 = Piece(TypePiece.SLAYER)
-    assert fight(p24,p23) == p23
+    assert fight(p24, p23) == p23
